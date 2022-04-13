1958b761655c615e2d10d1f4280b6c2acb7ffb6f
LI <921321018@qq.com>
2022-04-13 19:38:02 +0900

    clone

--- git-test-project/src/com/itheima/git.java
+++ git-test-project/src/com/itheima/git.java
@@ -5,9 +5,11 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //hello
     }
     public void test(){
         int i=0;
         i+=10;
+        System.out.println(i);
     }
 }
